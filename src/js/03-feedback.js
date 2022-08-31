1a62df994c27359a46abec122aa1beee322ed537
Reqvite <reqvite2233@gmail.com>
2022-08-31 18:04:37 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,36 @@
 import throttle from "lodash.throttle"
 const formRef = document.querySelector('form')
-const STORAGE_KEY = "feedback-form-state"
-let formData = {}
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
 
+const STORAGE_KEY = "feedback-form-state";
+let formData = {};
+
+checkLocalStorage()
 formRef.addEventListener('input', throttle(getFormValue, 500))
-
- function getFormValue(e) {
-     formData[e.target.name] = e.target.value
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
- }
-
 formRef.addEventListener('submit', (e) => {
     e.preventDefault()
-    if (!(formData.email && formData.message)) {
-       alert('All fields must be filled')
+      if (message.value === '' || email.value === '') {
+        alert(' Please fill all fields');
     } else {
-       e.currentTarget.reset()
-       localStorage.removeItem(STORAGE_KEY)
-        console.log(formData);
-        formData = {}
+       console.log(formData);
+          e.currentTarget.reset();
+          localStorage.removeItem(STORAGE_KEY);
     } 
 })
 
-checkLocalStorage()
+ function getFormValue(e) {
+     formData[e.target.name] = e.target.value;
+     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+ }
+
 function checkLocalStorage() {
-    const getValue = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    const getValue = JSON.parse(localStorage.getItem(STORAGE_KEY));
     if (getValue) {
-        formRef[0].value = getValue.email
-        formRef[1].value = getValue.message
-        formData = getValue
-    } 
+        formRef[0].value = getValue.email || '';
+        formRef[1].value = getValue.message || '';
+        formData = getValue;
+    }
 }
 
 
