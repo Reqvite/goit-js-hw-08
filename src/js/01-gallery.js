f592df69142e0bd584fdd0a40f3eaa5f38a71da2
Reqvite <reqvite2233@gmail.com>
2022-09-06 22:19:04 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -16,21 +16,3 @@ const createGalleryMarkup = galleryItems.map(item => {
 galleryContainer.insertAdjacentHTML('afterbegin', createGalleryMarkup)
  
 var lightbox = new SimpleLightbox('.gallery .gallery__item', {captionsData:'alt',captionDelay:250});
-
-// const refs = {
-//     galleryContainer: document.querySelector(".gallery"),
-// }
-
-// const galleryItemsMarkup = galleryItems.map(item => {
-//     const {preview, original, description} = item
-//     return `
-// <a class="gallery__item" href="${original}">
-//   <img class="gallery__image" src="${preview}" alt="${description}" />
-// </a>
-// `
-// }).join("")
-
-
-// refs.galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup)
-
-// var lightbox = new SimpleLightbox('.gallery .gallery__item', {captionsData:'alt',captionDelay:250});
